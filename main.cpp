e900f6556c59d2e2d1a278a4fc857f023d0cbaae
jykim <cognition@snu.ac.kr>
2025-10-08 17:04:30 +0900

    solve problem in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int cnt)
+{
+   if (vec.size() == M)
+   {
+      for (const auto element : vec)
+      {
+         cout << element << " ";
+      }
+      cout << "\n";
+   }
 
+   for (int i = cnt + 1; i <= N; i++)
+   {
+      vec.push_back(i);
 
+      func(++cnt);
 
-
-
-
-
+      vec.pop_back();
+   }
 }
 
-int main() {
+int main()
+{
    cin >> N >> M;
    func(0);
 }
